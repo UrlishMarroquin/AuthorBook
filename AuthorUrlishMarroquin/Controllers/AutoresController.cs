4e15cd9198dc7c38631e0369f683e6e0b51bf419
URLISH KLEYBER MARROQUIN MARROQUIN <dgiesp110@minsa.gob.pe>
2022-09-05 01:25:04 -0500

    Project Author Book

---
+++ AuthorUrlishMarroquin/Controllers/AutoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AuthorUrlishMarroquin.Models;
+
+namespace AuthorUrlishMarroquin.Controllers
+{
+    public class AutoresController : Controller
+    {
+        private AuthorDBContext db = new AuthorDBContext();
+
+        // GET: Autores
+        public ActionResult Index()
+        {
+            return View(db.Autores.ToList());
+        }
+
+        // GET: Autores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autore autore = db.Autores.Find(id);
+            if (autore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autore);
+        }
+
+        // GET: Autores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Autores/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AuthorID,AuthorName,AuthorCity,Genre,BookTitle,ReleaseYear,NumberOfPages,PiecesSold")] Autore autore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Autores.Add(autore);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(autore);
+        }
+
+        // GET: Autores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autore autore = db.Autores.Find(id);
+            if (autore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autore);
+        }
+
+        // POST: Autores/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AuthorID,AuthorName,AuthorCity,Genre,BookTitle,ReleaseYear,NumberOfPages,PiecesSold")] Autore autore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(autore).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(autore);
+        }
+
+        // GET: Autores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autore autore = db.Autores.Find(id);
+            if (autore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autore);
+        }
+
+        // POST: Autores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Autore autore = db.Autores.Find(id);
+            db.Autores.Remove(autore);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AuthorBook
+
+Creating Database with a Web Interface 
+Urlish Kleyber Marroquín Marroquín
